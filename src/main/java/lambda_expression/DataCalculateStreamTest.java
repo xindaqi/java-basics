a860b36b927a907432dad2bbdc5b6fa04f3a10f0
xdq101 <xdq101@qq.com>
2021-06-29 17:23:04 +0800

    Update. 添加stream数据计算测试样例。

--- src/main/java/lambda_expression/DataCalculateStreamTest.java
+++ src/main/java/lambda_expression/DataCalculateStreamTest.java
@@ -2,9 +2,7 @@ package lambda_expression;
 
 import common.entity.UserAgeEntity;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -45,10 +43,47 @@ public class DataCalculateStreamTest {
         logger.info("Sum:" + sum);
     }
 
+    /**
+     * 3.List<Double>average求均值
+     *
+     * @param doubleList 源列表
+     */
+    private static void averageTest(List<Double> doubleList) {
+        OptionalDouble average = Optional.ofNullable(doubleList).orElse(new ArrayList<>())
+                .stream()
+                .mapToDouble(Double::doubleValue)
+                .average();
+        if (average.isPresent()) {
+            double averageResult = average.getAsDouble();
+            logger.info("Avarege:" + averageResult);
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    /**
+     * 4.List<对象>average求均值
+     *
+     * @param userAgeEntityList 源列表
+     */
+    private static void averageTestWithObject(List<UserAgeEntity> userAgeEntityList) {
+        OptionalDouble average = Optional.ofNullable(userAgeEntityList).orElse(new ArrayList<>())
+                .stream()
+                .mapToInt(UserAgeEntity::getAge)
+                .average();
+        if (average.isPresent()) {
+            double averageResult = average.getAsDouble();
+            logger.info("Average:" + averageResult);
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
     public static void main(String[] args) {
 
         List<Double> doubleList = Stream.of(0.2, 0.3).collect(Collectors.toList());
         DataCalculateStreamTest.sumTest(doubleList);
+        DataCalculateStreamTest.averageTest(doubleList);
 
         List<UserAgeEntity> userAgeEntityList = new ArrayList<>();
         userAgeEntityList.add(new UserAgeEntity("1", "xiaoxiao", 10));
@@ -56,5 +91,6 @@ public class DataCalculateStreamTest {
         userAgeEntityList.add(new UserAgeEntity("3", "xiaolan", 12));
         userAgeEntityList.add(new UserAgeEntity("4", "xiaolan", 12));
         DataCalculateStreamTest.sumTestWithObject(userAgeEntityList);
+        DataCalculateStreamTest.averageTestWithObject(userAgeEntityList);
     }
 }
